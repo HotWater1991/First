5944114063356d34b820ebda9d3b3686751bc719
HotWater1991 <15993127580@163.com>
2018-05-14 16:55:21 +0800

    添加一个去除了干扰项的项目文件

---
+++ 180428SSM/src/main/java/com/zzu/service/impl/ItemServiceImpl.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.zzu.service.impl;
+
+import com.zzu.service.ItemService;
+
+/**  
+* <p>Title: ItemServiceImpl.java</p>  
+
+* <p>Description: </p>  
+
+ * <p>Company: zzu</p>  
+
+* @author HotWater  
+
+* @date 2018年4月28日  
+*/
+public class ItemServiceImpl implements ItemService {
+
+}
